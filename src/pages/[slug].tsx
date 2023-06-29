8e8616aed951739cc89825079a378295e3a1c004
Prasit.cpt <tempkaew@gmail.com>
2023-06-29 17:54:26 +0700

    change meta tag

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -36,20 +36,17 @@ const Page: React.FC<PageProps> = ({ pageData }) => {
   return (
     <>
       <Head>
+        <title>{pageData && pageData.t ? pageData.t : 'Blog'}</title>
+        <meta name="description" content={pageData && pageData.t ? pageData.t : 'Blog'} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content={pageData && pageData.t ? pageData.t : 'Blog'} />
+        <meta property="og:image" content={pageData && pageData.i ? pageData.i : 'Blog'} />
         {pageData && pageData?.b == true ? (
           <>
             <meta name="medium" content="image" />
-            <title>​</title>
-            <meta property="og:title" content="​" />
           </>
         ) : (
-          <>
-            <title>{pageData && pageData.t ? pageData.t : 'Blog'}</title>
-            <meta name="description" content={pageData && pageData.t ? pageData.t : 'Blog'} />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta property="og:title" content={pageData && pageData.t ? pageData.t : 'Blog'} />
-            <meta property="og:image" content={pageData && pageData.i ? pageData.i : 'Blog'} />
-          </>
+          <></>
         )}
         <link rel="icon" href="/favicon.ico" />
       </Head>
